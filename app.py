a01af1913f5e762eba594eec9ba222244a8ec1ed
Mouli Sivapurau <mouli@Moulis-MacBook-Air.local>
2017-02-04 17:26:18 -0800

    added tmsapi remove zip code

--- app.py
+++ app.py
@@ -45,10 +45,10 @@ def webhook():
     }
 
     print("aks2.5")
-    print(data[0]['title'])
+    #print(data[0]['title'])
     for movie_item in data:
         print(movie_item['title'])
-        if movie in movie_item['title']:
+        if movie_item['title'].find(movie):
             showtimes_theater = movie_item['showtimes']
             for stt in showtimes_theater:
                 if stt['theater']['name'] in theaters:
