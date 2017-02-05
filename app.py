52721ac4ec4614ecee39aed5ab1172c953c2efe3
Mouli Sivapurau <mouli@Moulis-MacBook-Air.local>
2017-02-04 18:06:30 -0800

    added tmsapi remove zip code

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ def webhook():
     #res = processRequest(req)
     result = req.get("result")
     parameters = result.get("parameters")
-    #zip_code = parameters.get("zip-code")
+    zip_code = parameters.get("zip-code")
     movie = parameters.get("movie")
 
     print("aks1")
 
-    baseurl = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2017-02-05&numDays=1&zip=94086&api_key=3dkbquh9qjwzd752hj8n2n79"
+    baseurl = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2017-02-05&numDays=1&zip="+zip_code+"&api_key=3dkbquh9qjwzd752hj8n2n79"
     result = urllib.request.urlopen(baseurl).read()
     data = json.loads(result)
 
